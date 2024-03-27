f061d496ef08e6a22dfae740c6582b4a5ce4bb65
Аркадий Орехов <aorehov@mobiledep.ru>
2024-03-27 11:02:09 +0300

    refactor: gallery

--- static/script.js
+++ static/script.js
@@ -2,6 +2,13 @@ document.addEventListener("click", function (e) {
   if (e.target.classList.contains("gallery-item")) {
     const src = e.target.getAttribute("src");
     document.querySelector(".modal-img").src = src;
+
+    content = document.querySelector(".opalubka-img-header").innerHTML;
+    document.querySelector(".modal-title").innerHTML = content;
+
+    content = document.querySelector(".opalubka-img-description").innerHTML;
+    document.querySelector(".modal-description").innerHTML = content;
+
     const myModal = new bootstrap.Modal(
       document.getElementById("gallery-popup")
     );
