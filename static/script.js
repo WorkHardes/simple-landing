ee8dbe7fa39d03b7d345aa7a837354ae15e04643
Аркадий Орехов <aorehov@mobiledep.ru>
2024-03-28 13:33:15 +0300

    refactor: add form email order

--- static/script.js
+++ static/script.js
@@ -1,3 +1,56 @@
+// Order
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("orderForm");
+  const sendButton = document.getElementById("sendButton");
+
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    try {
+      const formworkInput = form.elements["formworkType"];
+      const formworkTypeValue = formworkInput.value;
+      const totalAreaInput = form.elements["totalArea"];
+      const totalAreaValue = totalAreaInput.value;
+      const addressInput = form.elements["address"];
+      const addressValue = addressInput.value;
+      const customerNameInput = form.elements["customerName"];
+      const customerNameValue = customerNameInput.value;
+      const customerPhoneInput = form.elements["customerPhoneNumber"];
+      const customerPhoneValue = customerPhoneInput.value;
+      const customerEmailInput = form.elements["customerEmail"];
+      const customerEmailValue = customerEmailInput.value;
+
+      const response = await fetch("http://localhost:5000/api/v1/mail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          formwork_type: formworkTypeValue,
+          total_area: totalAreaValue,
+          address: addressValue,
+          customer_name: customerNameValue,
+          customer_phone_number: customerPhoneValue,
+          customer_email: customerEmailValue,
+        }),
+      });
+
+      if (response.ok) {
+        alert("Message sent successfully!");
+        form.reset();
+      } else {
+        throw new Error("Failed to send message.");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("An error occurred while sending the message.");
+    } finally {
+      sendButton.disabled = false;
+    }
+  });
+});
+
+// Gallery
 document.addEventListener("click", function (e) {
   if (e.target.classList.contains("gallery-item")) {
     const src = e.target.getAttribute("src");
